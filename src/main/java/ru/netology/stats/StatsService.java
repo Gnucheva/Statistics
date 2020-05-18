2a2c2a02674b348cc026a774570559b098d2c297
Anastasia <n41111215@mail.ru>
2020-05-18 17:28:01 +0300

    Добавила MaxMonthSales

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -9,4 +9,27 @@ public class StatsService {
         return AllSalesSum;
 
     }
+
+    public int calculateAverageSum(int[] monthlySales) {
+        int AllSalesSum = 0;
+        for (int monthlySale : monthlySales) {
+            AllSalesSum += monthlySale;
+        }
+        return AllSalesSum / monthlySales.length;
+
+    }
+
+    public int MaxMonthSales(int[] monthlySales) {
+        if (monthlySales == null || monthlySales.length == 0) return -1;
+        int month = 0;
+        for (int i = 1; i < monthlySales.length; i++) {
+            if (monthlySales[i] >= monthlySales[month]) month = i;
+        }
+        return month;
+    }
+
+
 }
+
+
+
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -14,4 +14,26 @@ class StatsServiceTest {
         int actual = service.calculateAllSalesSum(monthlySales);
         assertEquals(expected, actual);
     }
+
+    @Test
+    void calculateAverageSum() {
+        StatsService service = new StatsService();
+        int[] monthlySales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calculateAverageSum(monthlySales);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void MaxMonthSales() {
+        StatsService service = new StatsService();
+        int[] monthlySales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 7;
+        int actual = service.MaxMonthSales(monthlySales);
+        assertEquals(expected, actual);
+
+    }
+
+
 }
